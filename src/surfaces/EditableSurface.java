7909b29387b46f9a626445839eebc722ed7f917e
Joshua <Joshua@LaptopJoshua.wireless.unimaas.local>
2015-06-23 12:00:17 +0200

    Javadoc: -EditableSurface 		 -Labelizable

--- src/surfaces/EditableSurface.java
+++ src/surfaces/EditableSurface.java
@@ -1,8 +1,34 @@
 package surfaces;
 
+/**
+ * Interface which defines the methods for dynamically changeable surfaces.
+ */
 public interface EditableSurface {
+	
+	/**
+	 * Adds one vertex to the current model.
+	 * @param p Point to add to the vertices.
+	 * @param instruct instruction for the vertex
+	 */
 	public void addVertex(Point3D p, String instruct);
+	
+	/**
+	 * Adds two vertices to the current model.
+	 * @param p1 First point to add to the vertices.
+	 * @param p2 Second point to add to the vertices.
+	 * @param instruct instruction for the vertex
+	 * @param w Weight for the vertices.
+	 */
 	public void add2Vertices(Point3D p1, Point3D p2, String instruct, double w);
+	
+	/**
+	 * Adds two vertices to the current model.
+	 * @param p1 First point to add to the vertices.
+	 * @param p2 Second point to add to the vertices.
+	 * @param p3 Third point to add to the vertices.
+	 * @param instruct instruction for the vertex
+	 * @param w Weight for the vertices.
+	 */
 	public void add3Vertices(Point3D p1, Point3D p2, Point3D p3, 
 			String instruct, double w);
 }
--- src/surfaces/Labelizable.java
+++ src/surfaces/Labelizable.java
@@ -1,6 +1,17 @@
 package surfaces;
-
+/**
+ * Assigns labels to the instances of the objects.
+ */
 public interface Labelizable {
+	
+	/**
+	 * Assign a label to an object.
+	 */
 	void assignLabel();
+	
+	/**
+	 * Returns the label of an object.
+	 * @return the label of the object.
+	 */
 	String getLabel();
 }
